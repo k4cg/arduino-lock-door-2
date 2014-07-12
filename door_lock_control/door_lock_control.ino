2c4480fad32bdfc00b0be480b688a238193f445a
Sergio <sergiolindo.empresa@gmail.com>
2014-07-12 23:00:20 +0200

    Open serial. Servo pin 9. Emergency reset action.

--- door_lock_control/door_lock_control.ino
+++ door_lock_control/door_lock_control.ino
@@ -5,11 +5,23 @@
 // It listens at serial port for commands "open" and "close".
 
 #include <Servo.h>
-// The open and close position are the maximum and the minimum angular position for the servo.
+#define SERVOMOTOR_PIN 9
+#define SERIAL_BPS 9600 // bits per second.
+// The open and close position are the maximum and the minimum angular positions for the servo.
 // They depend on the servo. Interval for my servo [25,160].
 #define OPEN_POSITION 160 // degrees.
 #define CLOSE_POSITION 25 // degrees.
 
-void setup() {}
+Servo myservo;
+
+void setup() {
+  Serial.begin(SERIAL_BPS);
+  myservo.attach(SERVOMOTOR_PIN);
+
+  // Secure in case of emergency
+  myservo.write(OPEN_POSITION); // Unlock the door ...
+  delay(5000); // ... for 5 seconds ...
+  myservo.write(CLOSE_POSITION); // ... and close the door.
+}
 
 void loop() {}
