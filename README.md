8e5d198115091914af1f3e199fc8d49ec0943c59
Guenther Schmitz <727316+gpunktschmitz@users.noreply.github.com>
2019-10-12 19:12:44 +0200

    initial commit of "version 2.0"

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
-arduino-lock-door
-=================
+arduino-lock-door-2
+===================
 
-An arduino program to control the lock of a door (open and close).
-- It controls a servomotor attached to a lock.
-- It listens at serial port for commands "open" and "close".
+An arduino program to open the lock of a door.
+- It controls a servomotor (attached to the lock).
+- It listens at serial port for the command "open".
 
-![example photo](img/2014-07-13.jpg)
-
-[Link to web page](http://k4cg.github.io/arduino-lock-door/)
-
-[Link to the project "MatoLock"](https://k4cg.org/index.php?title=Projekt:MatoLock) (Nürnberg, Germany) Here was the project used to control a fridge door.
--- door_lock_control/door_lock_control.ino
+++ door_lock_control/door_lock_control.ino
@@ -1,78 +1,73 @@
-// Door lock control
-// By sergiolindo.empresa@gmail.com
-//
-// It controls a servomotor attached to a lock.
-// It listens at serial port for commands "open" and "close".
-
-#include <Servo.h>
-#define SERVOMOTOR_PIN 9
+#include "Servo.h"
+#define SERVOMOTOR_PIN 11
 #define SERIAL_BPS 9600 // bits per second.
-// The open and close position are the maximum and the minimum angular positions for the servo.
-// They depend on the servo. Interval for my servo [25,160].
-#define OPEN_POSITION 160 // degrees.
-#define CLOSE_POSITION 25 // degrees.
-
+#define OPEN_POSITION 60 // degrees.
+#define CLOSE_POSITION 150 // degrees.
 Servo myservo;
-String command = ""; // open | close
+String command = ""; // available commands: "open"
+
+int WAIT_LONG = 7000;
+int WAIT_SHORT = 200;
+
+int CLOSE_DELAY = 750;
+
+int pushButton = 2;
+
+int buttonState; // 0 = closed; 1 = open
+int previousButtonState;
+int openState = 0;
 
 void setup() {
   Serial.begin(SERIAL_BPS);
-  Serial.println("setup begins");
-  Serial.print("  Serial communication port opened at ");
-  Serial.println(SERIAL_BPS);
-
   myservo.attach(SERVOMOTOR_PIN);
-  Serial.print("  Servo attached to pin ");
-  Serial.println(SERVOMOTOR_PIN);
-  Serial.println();
-
-  Serial.println("  Secure in case of emergency");
-  Serial.print("  Set servo to ");
-  Serial.print(OPEN_POSITION);
-  Serial.println(" (open)");
-  myservo.write(OPEN_POSITION); // Unlock the door ...
-  Serial.println("  Waiting 5 seconds");
-  delay(5000); // ... for 5 seconds ...
-  Serial.print("  Set servo to ");
-  Serial.print(CLOSE_POSITION);
-  Serial.println(" (close)");
-  myservo.write(CLOSE_POSITION); // ... and lock the door.
-  delay(500);
-
-  Serial.println("setup ends");
-  Serial.println();
+  myservo.write(OPEN_POSITION);
+  delay(WAIT_LONG);
+  myservo.write(CLOSE_POSITION);
+  delay(WAIT_SHORT);
+  
+  pinMode(pushButton, INPUT_PULLUP);
 }
 
 void loop() {
   char letter;
+  previousButtonState = buttonState;
 
-  if (Serial.available() > 0) {
-    letter = Serial.read(); // 1 Byte = 1 char = 1 letter.
-    Serial.print("\"");
-    Serial.print(command);
-    Serial.print("\" < '");
-    Serial.print(letter);
-    command.concat(letter);
-    Serial.print("' (\"");
-    Serial.print(command);
-    Serial.print("\", ");
-    Serial.print(command.length());
-    Serial.println(")");
-
-    if (command == "close") {
-      Serial.println("Motor on (close)");
-      myservo.write(CLOSE_POSITION);
-      delay(500);
-    }
-
-    if (command.length() > 4) {
-      command = command.substring(1);
-    }
-
-    if (command == "open") {
-      Serial.println("Motor on (open)");
-      myservo.write(OPEN_POSITION);
-      delay(500);
-    }
+  // when door gets unlocked (was closed) keep door unlocked until it is opened
+  buttonState = digitalRead(pushButton);
+  while(buttonState == 0 && openState == 1) {
+    delay(WAIT_SHORT);
+    buttonState = digitalRead(pushButton);
+    delay(CLOSE_DELAY);
   }
+
+  // add delay 
+  if(previousButtonState == 1 && buttonState == 0) {
+    delay(CLOSE_DELAY);
+  }
+
+  openState = 0;
+  
+  if(buttonState == 0) {
+    delay(WAIT_SHORT);
+    myservo.write(CLOSE_POSITION);
+    
+    if(Serial.available() > 0) {
+      letter = Serial.read(); // 1 Byte = 1 char = 1 letter.
+      command.concat(letter);
+  
+      if(command.length() > 4) {
+        command = command.substring(1);
+      }
+  
+      if(command == "open") {
+        myservo.write(OPEN_POSITION);
+        openState = 1;
+      }
+    }
+  } else {
+    delay(WAIT_SHORT);
+    myservo.write(OPEN_POSITION);
+  }
+
+  delay(WAIT_SHORT);
 }
